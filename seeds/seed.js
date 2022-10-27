5d252efbf2ecb968df98cb2dc2260f79686e73ba
AaronVenema <34764586+AaronVenema@users.noreply.github.com>
2022-10-27 10:47:23 -0500

    seeds made

---
+++ seeds/seed.js
@@ -0,0 +1,23 @@
+const sequelize = require('../config/connection');
+const { User, Bill, Category, Income  } = require('../models');
+
+const userData = require('./userData.json');
+const billData = require('./billData.json');
+const categoryData = require('./categoryData.json');
+const incomeData = require('./incomeData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    // individualHooks: true,
+    // returning: true,
+  });
+  const bills = await Bill.bulkCreate(billData, {
+  });
+  const categories = await Category.bulkCreate(categoryData, {
+  });
+  const incomes = await Income.bulkCreate(incomeData, {
+  });
+};
+seedDatabase();
