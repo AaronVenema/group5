b0662c854ea7f13ca8b691d7edff9763ca13a0ed
Brent Gustafson <50030696+BrentG6000@users.noreply.github.com>
2022-10-28 14:20:55 -0500

    Added a helper file

--- helper/budgetmath.js
+++ helper/budgetmath.js
@@ -1,1 +1,35 @@
-const 
+const monthlyBudget = (bills, incomes) => {
+    let expenses;
+    let income;
+
+    expenses = allExpenses(bills);
+    income = allIncome(incomes);
+    
+    return income - expenses;
+}
+
+const allExpenses = (bills) => {
+    let expenses = 0;
+
+    for (let i = 0; i < bills.length; i++) {
+        expenses = expenses + bills[i].amount;
+    }
+    
+    return expenses;
+}
+
+const allIncome = (incomes) => {
+    let income = 0;
+
+    for (let i = 0; i < incomes.length; i++) {
+        income = income + income[i].amount;
+    }
+
+    return income;
+}
+
+module.exports = {
+    monthlyBudget,
+    allExpenses,
+    allIncome
+}
