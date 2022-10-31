82cd3bde0d9a84bcc455a24503fb06c6b164611b
AaronVenema <34764586+AaronVenema@users.noreply.github.com>
2022-10-31 14:00:33 -0500

    were are grooving and moovin

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -1,4 +1,9 @@
 const router = require('express').Router();
+
+router.get('/', (req, res) => {
+  res.render('home');
+});
+
 router.get('/login', (req, res) => {
   if (req.session.logged_in) {
     res.redirect('/login');
