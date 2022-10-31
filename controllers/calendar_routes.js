cfe04a5552964aaeb36a65aaacecf41b32dcf5c0
Glitch0320 <100811040+Glitch0320@users.noreply.github.com>
2022-10-31 10:26:11 -0500

    update

--- controllers/calendar_routes.js
+++ controllers/calendar_routes.js
@@ -1,15 +1,15 @@
 const router = require('express').Router();
-const {Category} = require('../models')
+const { Category } = require('../models')
 // 3001/dashboard
 
 router.get('/', async (req, res) => {
     try {
         // Pass serialized data and session flag into template
         res.render('calendar',
-        {
-            logged_in: req.session.logged_in,
-            user_id: req.session.user_id
-        }
+            {
+                logged_in: req.session.logged_in,
+                user_id: req.session.user_id
+            }
         );
     } catch (err) {
         res.status(500).json(err);
@@ -18,20 +18,21 @@ router.get('/', async (req, res) => {
 
 // save date to req.session
 router.post('/event', (req, res) => {
+    console.log('found')
     req.session.save(() => {
         req.session.dateStr = req.body.dateStr
     })
 
-    res.redirect(req.body.path)
+    res.redirect('/event')
 })
 
 // render forms
 router.get('/event', async (req, res) => {
     const categoryData = await Category.findAll()
-    const categories = categoryData.map(c => c.get({plain:true}))
+    const categories = categoryData.map(c => c.get({ plain: true }))
     res.render('event', {
         categories: categories,
-        dateStr: req.session.dateStr
+        // dateStr: req.session.dateStr
     })
 })
 
--- controllers/index.js
+++ controllers/index.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
-router.use('/dashboard', require('./calendar_routes'))
+router.use(require('./calendar_routes'))
 
 module.exports = router;
