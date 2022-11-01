f47f93f5c207316942712b5d78bb6e2a7709472b
Glitch0320 <100811040+Glitch0320@users.noreply.github.com>
2022-11-01 11:15:06 -0500

    more bug fixing

--- controllers/EXAMPLE.js
+++ controllers/EXAMPLE.js
@@ -1,63 +1,87 @@
 const router = require('express').Router();
 
 // HOME ROUTES
+router.get('/', (req, res) => {
+  res.render('home');
+});
+
 router.get('/login', (req, res) => {
-    if (req.session.logged_in) {
-      res.redirect('/login');
-      return;
-    }
-    res.render('login');
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
+  res.render('login');
 });
 
 router.get('/signup', (req, res) => {
-if (req.session.logged_in) {
-    res.redirect('/signup');
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
     return;
-}
-res.render('signup');
+  }
+  res.render('signup');
 });
 
 // CALENDAR ROUTES
 
 router.get('/dashboard/', async (req, res) => {
-    try {
-        const billData = await Bill.findAll({
-            attributes: [['name', 'title'], 'amount', ['date_str', 'start']]
-        })
-        const bills = billData.map(bill => bill.get({ plain: true }))
-        const incomeData = await Income.findAll({
-            attributes: [['name', 'title'], 'amount', ['date_str', 'start']]
-        })
-        const incomes = incomeData.map(income => income.get({ plain: true }))
-        const events = [] 
-        bills.forEach(bill => events.push(bill))
-        incomes.forEach(income => events.push(income))
-        // Pass serialized data and session flag into template
-        res.render('calendar',
-            {
-                logged_in: req.session.logged_in,
-                user_id: req.session.user_id,
-                events: JSON.stringify(events)
-            }
-        );
-    } catch (err) {
-        res.status(500).json(err);
-    }
+  console.log(req.session)
+  try {
+      const billData = await Bill.findAll({
+          where: {id:req.session.user_id},
+          attributes: [['name', 'title'], 'amount', ['date_str', 'start']]
+      })
+      const bills = billData.map(bill => bill.get({ plain: true }))
+      const incomeData = await Income.findAll({
+          where: {id:req.session.user_id},
+          attributes: [['name', 'title'], 'amount', ['date_str', 'start']]
+      })
+      const incomes = incomeData.map(income => income.get({ plain: true }))
+      const events = [] 
+      let exp = 0
+      let inc = 0
+      bills.forEach(bill => {
+          bill.backgroundColor = '#f78d46'
+          bill.type = 'bill'
+          exp += bill.amount
+          events.push(bill)
+      })
+      incomes.forEach(income => {
+          income.backgroundColor = '#55c937'
+          income.type = 'income'
+          inc += income.amount
+          events.push(income)
+      })
+      const username = 'Jon'
+      let disposableIncome = inc - exp
+      // Pass serialized data and session flag into template
+      res.render('calendar',
+          {
+              layout: 'dashboard',
+              logged_in: req.session.logged_in,
+              username,
+              disposableIncome,
+              user_id: req.session.user_id,
+              events: JSON.stringify(events)
+          }
+      );
+  } catch (err) {
+      res.status(500).json(err);
+  }
 });
-
+s
 router.get('/dashboard/event/:date', async (req, res) => {
-    try {
-        const categoryData = await Category.findAll()
-        const categories = categoryData.map(c => c.get({ plain: true }))
-        console.log(categories, req.params.date)
-        res.render('event', {
-            categories,
-            dateStr: req.params.date
-        })
-        return
-    } catch (err) {
-        res.status(500).json(err);
-    }
+  try {
+      const categoryData = await Category.findAll()
+      const categories = categoryData.map(c => c.get({ plain: true }))
+      console.log(categories, req.params.date)
+      res.render('event', {
+          categories,
+          dateStr: req.params.date
+      })
+      return
+  } catch (err) {
+      res.status(500).json(err);
+  }
 })
 
 // API
@@ -66,46 +90,45 @@ router.get('/dashboard/event/:date', async (req, res) => {
 router.post('/api/users/signup', async (req, res) => {
   try {
     const userData = await User.create(req.body);
-
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
 
       res.status(200).json(userData);
-    });
+    }
+    )
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
+
 router.post("/api/users/login", async (req,res) => {
-
+  console.log('hit')
   try {
-
     const userData = await User.findOne({ where: { email: req.body.email } });
 
     if (!userData) {
-      res
-        .status(400)
-        .json({ message: "Incorrect email, please try again." });
+      res.status(400).json({ message: "Incorrect email, please try again." });
       return;
     }
+    const user = userData.get({plain:true})
+    console.log(user)
 
-    const validPassword = await userData.checkPassword(req.body.password);
+    const validPassword = await user.checkPassword(req.body.password);
     console.log(`>>>validPassword is ${validPassword}`);
 
     if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: "Incorrect password, please try again." });
+      res.status(400).json({ message: "Incorrect password, please try again." });
       return;
     }
 
     req.session.save(() => {
-      req.session.user_id = userData.id;
+      req.session.user_id = user.id;
       req.session.logged_in = true;
     });
-    res.status(200).json({ user: userData, message: "You are now logged in!" });
+
+    res.status(200).json({ user, message: "You are now logged in!" });;
 
   } catch (err) {
     res.status(400).json(err);
@@ -121,6 +144,7 @@ router.post("/api/users/logout", async (req,res) => {
     res.status(404).end();
   }
 });
+
 
 router.put('/api/users/:id', async (req, res) => {
   try {
@@ -182,7 +206,7 @@ router.post('/api/bills/', async (req, res) => {
   try {
       const categoryId = await Category.findOne({where:{name:req.body.category},attributes:['id']})
       req.body.category_id = categoryId.dataValues.id
-      // req.body.user_id = req.session.user_id
+      req.body.user_id = req.session.user_id
       console.log(req.body)
       const BillData = await Bill.create(req.body);
       res.status(200).json(BillData);
@@ -331,7 +355,7 @@ router.post('/api/incomes/', async (req, res) => {
           // amount
           // dateStr
       // }
-      // req.body.user_id = req.session.user_id
+      req.body.user_id = req.session.user_id
       const incomeData = await Income.create(req.body);
       res.status(200).json(incomeData);
   } catch (err) {
@@ -361,7 +385,7 @@ router.put('/api/incomes/:id', async (req, res) => {
   }
 });
 
-router.delete('/api/incomes/:id', async (req, res) => {
+router.delete('/api/incomes/api/:id', async (req, res) => {
   try {
       const incomeData = await Income.destroy({
           where: {
